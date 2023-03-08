4dace1f38ad904cbf51006963b759d898e154836
LICIA SANTOS <liciasantos028@gmail.com>
2023-03-08 08:55:38 -0300

    projeto

---
+++ ConsoleApp_manha/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_manha
+{
+    internal class Produto
+    {
+        public int Id;
+        public string Descricao;
+        public decimal Valor;
+
+        public Produto(int id, string descricao, decimal valor)
+        {
+            Id = id; 
+            Descricao = descricao;
+            Valor = valor; 
+        }
+    }
+}
